bfbfd7a78cd151e359487381de20b0f20d7488d1
Bruno Bachmann <bfbachmann@gmail.com>
2016-04-11 00:15:46 -0700

    Teaked character size.

--- Skydiver/Skydiver/MainCharacter.cpp
+++ Skydiver/Skydiver/MainCharacter.cpp
@@ -24,11 +24,11 @@ void MainCharacter::render() {
     glBegin(GL_TRIANGLES);
     
     glColor3f(1.f, 0.f, 0.f);
-    glVertex3f(x-0.6f, y-0.4f, 0.f);
+    glVertex3f(x-0.2f, y-0.1f, 1.0f);
     glColor3f(0.f, 1.f, 0.f);
-    glVertex3f(x+0.6f, y-0.4f, 0.f);
+    glVertex3f(x+0.2f, y-0.1f, 1.0f);
     glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(x+0.f, y+0.6f, 0.f);
+    glVertex3f(x+0.f, y+0.2f, 1.0f);
     
     glEnd();
 }
