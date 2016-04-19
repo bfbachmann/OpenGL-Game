879eb54e0746f0f99af61a380545256960a7a292
Bruno Bachmann <bfbachmann@gmail.com>
2016-04-18 23:39:33 -0700

    Fixed character movement problems, added TODO

--- Skydiver/Skydiver/MainCharacter.cpp
+++ Skydiver/Skydiver/MainCharacter.cpp
@@ -7,6 +7,8 @@
 //
 
 #include "MainCharacter.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 
 MainCharacter::MainCharacter(float x, float y) {
@@ -18,19 +20,20 @@ MainCharacter::MainCharacter(float x, float y) {
 
 void MainCharacter::render() {
         
-    int x = location->getX();
-    int y = location->getY();
+    float x = location->getX();
+    float y = location->getY();
     
     glBegin(GL_TRIANGLES);
     
     glColor3f(1.f, 0.f, 0.f);
-    glVertex3f(x-0.2f, y-0.1f, 1.0f);
+    glVertex2f(x-0.2f, y-0.1f);
     glColor3f(0.f, 1.f, 0.f);
-    glVertex3f(x+0.2f, y-0.1f, 1.0f);
+    glVertex2f(x+0.2f, y-0.1f);
     glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(x+0.f, y+0.2f, 1.0f);
+    glVertex2f(x+0.f, y+0.2f);
     
     glEnd();
+    glFlush();
 }
 
 
